e7d75f88fab2dc0d559c3bdd2b959526af972f80
John Amakye <john.amakye@minneapolismn.gov>
2016-12-05 14:13:50 -0600

    got some functionality to work

--- scripts/client.js
+++ scripts/client.js
@@ -3,26 +3,30 @@ console.log("javascript is here!!!");
 $(document).ready(function(){
 console.log("jquery is here!!!");
 
-//begin click function
+var studentArray = []; //array to store object info.tau into,
+
+// begin click function
 $("#clickMe").on("click", function(){
 //begin ajax
 $.ajax({
-  dataType: "JSON",
-  url: "http://devjana.net/support/tau_students.json",
-  success: function(info){
-  console.log("ajax success:", info);
-  displayOnDOM(info.tau);
-} //end info
-}); //end ajax
-});// end click function
-
+    dataType: "JSON",
+    url: "http://devjana.net/support/tau_students.json",
+    success: function(info){
+    console.log("ajax success:", info);
+    studentArray = info.tau; //store info.tau in global var
+    displayOnDOM(info.tau);
+  } //end info
+  }); //end ajax
+console.log("in student array:", studentArray);//logging what is in student array
 //begin display
-var displayOnDOM = function(tauStudents){
-  $("#showMe").empty();
-  for( var i = 0 ; i < tauStudents.length; i++ ){
-    $( "#showMe").append( "<p> Student Name: <br>" + tauStudents[ i ].first_name + " " + tauStudents[ i ].last_name);
-    $( "#showMe").append( "<p> Information: <br>" + tauStudents[i].info + "</p>");
-    $( "#showMe").append( "Picture: <img src= " + tauStudents[ i ].picUrl +  ">");
-}
-};//end display
+  var displayOnDOM = function(tauStudents){
+    $("#showMe").empty();
+    for( var i = 0 ; i < tauStudents.length; i++ ){
+      console.log("In tauStudents[i]:",tauStudents[i]);
+      $( "#showMe").append( "Picture: <img src= " + tauStudents[ i ].picUrl +  ">");
+      $( "#showMe").append( "<p> Student Name: <br>" + tauStudents[ i ].first_name + " " + tauStudents[ i ].last_name);
+      $( "#showMe").append( "<p> Information: <br>" + tauStudents[i].info + "</p>");
+  }
+  };//end display
+});// end click function
 });//end document ready
---
+++ scripts/client2.js
@@ -0,0 +1,52 @@
+
+console.log("javascript is here!!!");
+$(document).ready(function(){
+console.log("jquery is here!!!");
+
+var studentArray = []; //array to store object info.tau into,
+var studentIndex = 0;  //which student are we at
+var studentLength = 0;//what is the length of our array
+
+
+//begin ajax
+$.ajax({
+    dataType: "JSON",
+    url: "http://devjana.net/support/tau_students.json",
+    success: function(studentData){
+    console.log("ajax success:", studentData);
+    studentArray = studentData.tau;  //store studentData.tau in global var
+    studentLength = studentArray.length; //store studentData length in global var
+  } //end studentData
+}); //end ajax
+
+console.log("in student array:", studentArray);//logging what is in student array
+
+ //nextStudent()
+  $("#next").on("click", function(){
+  next(studentArray[studentIndex++]);
+});
+
+  //prevStudent()
+  $("#prev").on("click", function(){
+prev(studentArray[studentIndex--]);
+});
+
+  //begin next display
+  var next = function(tauStudents){
+    $(".show-Me").empty();
+      console.log("In tauStudents index:",tauStudents);
+      $( ".show-Me").append( "Picture: <img src= " + tauStudents.picUrl +  ">");
+      $( ".show-Me").append( "<p> Student Name: <br>" + tauStudents.first_name + " " + tauStudents.last_name);
+      $( ".show-Me").append( "<p> Information: <br>" + tauStudents.info + "</p>");
+  };//end  next display
+
+  //begin prev display
+  var prev = function(tauStudents){
+    $(".show-Me").empty();
+      console.log("In tauStudents index:",tauStudents);
+      $( ".show-Me").append( "Picture: <img src= " + tauStudents.picUrl +  ">");
+      $( ".show-Me").append( "<p> Student Name: <br>" + tauStudents.first_name + " " + tauStudents.last_name);
+      $( ".show-Me").append( "<p> Information: <br>" + tauStudents.info + "</p>");
+  };//end  prev display
+
+});//end document ready
